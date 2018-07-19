a9d969ece6581f5dccbd34a2c29d4767ff0417c4
huochen <huochen@pinduoduo.com>
2018-07-19 16:33:17 +0800

    生成spiderd的命令

---
+++ scrapy_study/louspider/louspider/spiders/example.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+# scrapy genspider example example.com
+class ExampleSpider(scrapy.Spider):
+    name = 'example'
+    allowed_domains = ['example.com']
+    start_urls = ['http://example.com/']
+
+    def parse(self, response):
+        pass
